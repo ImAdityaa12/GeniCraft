65ba8102b5841ed47bb9f8dfffc9cbfa66687ad5
ImAdityaa12 <117254771+ImAdityaa12@users.noreply.github.com>
2023-09-10 11:35:11 +0530

    updated

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -50,11 +50,6 @@ const routes = [
         href: "/code",
         color: "text-green-700",
     },
-    {
-        label: "Settings",
-        icon: Settings,
-        href: "/settings",
-    },
 ]
 
 const Sidebar = () => {
